15586973739e7a752b25562a4dd3070c313adbdc
Philip Molloy <philip@philipmolloy.com>
2025-04-18 12:36:45 +0200

    github: Ignore line length in tables
    Signed-off-by: Philip Molloy <philip@philipmolloy.com>

--- .mdl_style.rb
+++ .mdl_style.rb
@@ -1,6 +1,6 @@
 all
-# Ignore line length in code blocks
-rule 'MD013', :ignore_code_blocks => true
+# Ignore line length in code blocks and tables
+rule 'MD013', :ignore_code_blocks => true, :tables => false
 # Allow inline HTML
 exclude_rule 'MD033'
 # Order ordered lists
