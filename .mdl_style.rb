57eb9352654ceba9e387dfe60361fd96b66d6fcd
Daniel Maslowski <info@orangecms.org>
2025-09-16 01:53:05 +0200

    mdl style: set indent to 2 spaces
    Signed-off-by: Daniel Maslowski <info@orangecms.org>

--- .mdl_style.rb
+++ .mdl_style.rb
@@ -1,4 +1,6 @@
 all
+# This should be 2 by default per the docs, but turned out to be 8
+rule 'MD007', :indent => 2
 # Ignore line length in code blocks and tables
 rule 'MD013', :ignore_code_blocks => true, :tables => false
 # Allow inline HTML
